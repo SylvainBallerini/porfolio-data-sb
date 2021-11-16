aae2b450fe54d76594394b82a19fcf6ab5151b6e
SylvainBallerini <ballerini.sylvain@gmail.com>
2021-11-16 11:05:11 +0100

    maj page streamlit

--- apps/projets_streamlit.py
+++ apps/projets_streamlit.py
@@ -13,8 +13,9 @@ import pandas as pd
 layout = [html.H2("Projets Streamlits"),
 html.P("Streamlit est un framework open-source Python spécialement conçu pour les Data Analyst et les Data scientists.\
     Ce framework permet de créer des applications web qui pourront intégrer aisément des modèles de machine learning et des outils de visualisation de données."),
-html.H3("Dashboard COVID 19"),
-html.P("Dashborad en construction sur l'évolution de la pandémie avec différents graphiques interactives sur l'évolution de la maladie"),
-dcc.Link("Dashboard Covid-19", href="https://share.streamlit.io/sylvainballerini/covid/main/app.py", target="blank")
+html.H3("Dashboard Streamlit : il contient un ensemble de différents projets personnel avec de la datavisualition et du Machine Learning"),
+html.P("Covid : dashboard en construction sur l'évolution de la pandémie avec différents graphiques interactifs sur l'évolution de la maladie"),
+html.P("Transport en Europe : dashboard interactif sur une analyse des différent moyen de transport en Europe (bus, train, et covoiturage) "),
+dcc.Link("Dashboard Streamlit", href="https://share.streamlit.io/sylvainballerini/porfolio_streamlit/main/app.py", target="blank")
 ]
 
